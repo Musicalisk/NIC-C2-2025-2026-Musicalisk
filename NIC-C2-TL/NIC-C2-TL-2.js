34006f61429502eeef0c4440952c9345c779dfc9
Musicalisk <musicalisk.travail@tuta.io>
2025-11-05 11:31:10 -0500

    Worked on assignment

--- NIC-C2-TL/NIC-C2-TL-2.js
+++ NIC-C2-TL/NIC-C2-TL-2.js
@@ -6,5 +6,22 @@ window.onload = () =>  {
     let monthNames = ["January","Febuary","March","April","May","June","July","August","September","October","November","Decemeber"];
     let day = today.getDate();
     let year = today.getFullYear();
-    // document.getElementById("date").innerHTML = dayNames[day]
+    document.getElementById("date").innerHTML = dayNames[dayNumber] + '<br>' + monthNames[monthNumber] + " " + day + ', ' + year;
+}
+let itemArray = [];
+function buttonOne(){
+    let inputOne = document.getElementById("inputone").value;
+    if(inputOne.length == 0){
+        alert('01');
+    }
+    else{
+        let first = inputOne.substr(0, 1);
+        let last = inputOne.substr(1);
+        first = first.toUpperCase();
+        last = last.toLowerCase();
+        inputOne = first + last;
+        itemArray.push(inputOne);
+        document.getElementById("inputform").reset();
+        alert('02');
+   }
 }
